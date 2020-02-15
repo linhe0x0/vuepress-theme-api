c939fc298d01097d87ec17fb5d773c6a47ce5a86
sqrtthree <imsqrtthree@gmail.com>
2020-02-16 00:26:42 +0800

    feat: Enable active-header-links plugin

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Noop.
-
-// This file is required due to publish the theme as an npm package.
-// More details are https://v1.vuepress.vuejs.org/theme/writing-a-theme.html#directory-structure
-
-module.exports = {}
+module.exports = {
+  plugins: ['@vuepress/active-header-links'],
+}
