e316b5ac491dd55d5febed81a44c46f3ed1b67b5
sqrtthree <imsqrtthree@gmail.com>
2020-03-06 18:01:23 +0800

    feat: Add supports for container and smooth-scroll plugins

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 module.exports = {
-  plugins: ['@vuepress/active-header-links'],
+  plugins: [
+    '@vuepress/active-header-links',
+    [
+      'container',
+      {
+        type: 'tip',
+        defaultTitle: {
+          '/': 'TIP',
+          '/zh/': '提示',
+        },
+      },
+    ],
+    [
+      'container',
+      {
+        type: 'warning',
+        defaultTitle: {
+          '/': 'WARNING',
+          '/zh/': '注意',
+        },
+      },
+    ],
+    [
+      'container',
+      {
+        type: 'danger',
+        defaultTitle: {
+          '/': 'WARNING',
+          '/zh/': '警告',
+        },
+      },
+    ],
+    [
+      'container',
+      {
+        type: 'details',
+        before: info =>
+          `<details class="custom-block details">${
+            info ? `<summary>${info}</summary>` : ''
+          }\n`,
+        after: () => '</details>\n',
+      },
+    ],
+    ['smooth-scroll', true],
+  ],
 }
